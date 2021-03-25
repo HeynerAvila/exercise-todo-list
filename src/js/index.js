5be8757f828b12ab50deaeaa9704899ce2115511
HeynerAvila <heineravila@gmail.com>
2021-03-25 00:09:00 +0000

    Actualizacion

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,5 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./component/app";
 
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
-//include your index.scss file into the bundle
-import "../styles/index.scss";
-
-//import your own components
-import { Home } from "./component/home.js";
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.getElementById("todolist"));
